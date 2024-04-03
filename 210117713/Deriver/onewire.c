55555207c8bad744bb78f7c459b690371502f255
layyhot <137988909+layyhot@users.noreply.github.com>
2024-04-03 16:43:15 +0800

    Add files via upload

---
+++ 210117713/Deriver/onewire.c
@@ -0,0 +1,59 @@
+#include "onewire.h"
+
+//?—¿ß??ƒ®çæ?èb
+void Delay_OneWire(unsigned int t)  
+{
+	while(t--);
+}
+
+//?—¿ß®s?÷[
+void Write_DS18B20(unsigned char dat)
+{
+	unsigned char i;
+	for(i=0;i<8;i++)
+	{
+		DQ = 0;
+		DQ = dat&0x01;
+		Delay_OneWire(50);
+		DQ = 1;
+		dat >>= 1;
+	}
+	Delay_OneWire(50);
+}
+
+//?—¿ß??÷[
+unsigned char Read_DS18B20(void)
+{
+	unsigned char i;
+	unsigned char dat;
+  
+	for(i=0;i<8;i++)
+	{
+		DQ = 0;
+		dat >>= 1;
+		DQ = 1;
+		if(DQ)
+		{
+			dat |= 0x80;
+		}	    
+		Delay_OneWire(50);
+	}
+	return dat;
+}
+
+//DS18B20?åÈ?
+bit init_ds18b20(void)
+{
+  	bit initflag = 0;
+  	
+  	DQ = 1;
+  	Delay_OneWire(120);
+  	DQ = 0;
+  	Delay_OneWire(800);
+  	DQ = 1;
+  	Delay_OneWire(100); 
+    initflag = DQ;     
+  	Delay_OneWire(50);
+  
+  	return initflag;
+}
